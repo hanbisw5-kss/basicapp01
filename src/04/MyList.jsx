cb16dd6ec0886faaa0bf1a7866c210f28b324d0b
hanbisw5-kss <hanbisw5@gmail.com>
2026-02-23 21:13:59 +0900

    02/23

---
+++ src/04/MyList.jsx
@@ -0,0 +1,11 @@
+import MyListData from './MyListData.json';
+
+
+export default function MyList() {
+    console.log(MyListData)
+  return (
+    <div>
+      MyList
+    </div>
+  )
+}
--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react'
+// import logo from './logo.svg';
 import './App.css';
+import { FcCopyright } from "react-icons/fc";
+import { IoHome } from "react-icons/io5";
+// import MyDiv1 from './03/MyDiv1';
+import MyList from './04/MyList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js kkkkk</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col w-full h-screen mx-auto ">
+      
+     <header className='flex justify-between items-center text-x1 font-bold h-20 p-10 bg-slate-200'>
+            <p>리액트 기초 학습</p>
+            <p><IoHome /></p>
+     </header>
+     <main className='grow w-full flex justify-center items-center overflow-y-auto'>
+          <MyList />
+     </main>
+     <footer className='flex justify-center items-center h-20 bg-black text-slate-100'>
+          <FcCopyright /> Kim Seong Sik 01
+     </footer>
     </div>
   );
 }
